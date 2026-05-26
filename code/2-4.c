0e28a0f4350da5d227c2658b02a3c2ca3d58f9d4
cnu202502195 <202502195@o.cnu.ac.kr>
2026-05-26 13:53:27 +0900

    2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -3,11 +3,26 @@
 int main(int argc, const char* argv[]) {
     if (argc < 2)
         return 0;
-
+    
     FILE* fp = fopen(argv[1], "r");
 
+    if(fp == NULL) 
+        return 0;
 
+    int count[26] = {0};
+    char ch;
+
+    while((ch = fgetc(fp)) != EOF) {
+        if(ch >= 'a' && ch <= 'z') {
+            count[ch - 'a']++;
+        }
+    }
+
+    for(int i=0;i<26;i++) {
+        printf("%c: %d\n", 'a' + i, count[i]);
+    }
 
     fclose(fp);
-}
 
+    
+}
