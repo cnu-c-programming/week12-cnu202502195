195251de2dfc8e558668440ce3e38f5cb29a2c65
cnu202502195 <202502195@o.cnu.ac.kr>
2026-05-26 14:14:54 +0900

    2-7.c

--- code/2-7.c
+++ code/2-7.c
@@ -11,6 +11,46 @@ typedef struct config {
 
 void config_parser(Config* config_ptr) {
 
+}#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct config {
+    char InputFileName[64];
+    int Options;
+    char SectionName[64];
+    unsigned long long Address;
+} Config;
+
+void config_parser(Config* config_ptr) {
+    FILE* fp = fopen("config.txt", "r");
+
+    fscanf(fp, "InputFileName=%s",
+            config_ptr->InputFileName);
+    
+    fscanf(fp, "Options=%d",
+            &config_ptr->Options);
+
+    fscanf(fp, "SectionName=%s", 
+            config_ptr->SectionName);
+
+    fscnaf(fp, "Address=%llx",
+            &config_ptr->Address);
+    fclose(fp);
+
+}
+
+int main(int argc, const char* argv[]) {
+    Config config;
+    config_parser(&config);
+
+    printf("config: %s %d %s %llu\n",
+        config.InputFileName,
+        config.Options,
+        config.SectionName,
+        config.Address);
+    
+    return 0;
 }
 
 int main(int argc, const char* argv[]) {
