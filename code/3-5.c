a829948aa2dd4791b8515acef6bd70540929e85a
cnu202502195 <202502195@o.cnu.ac.kr>
2026-05-26 14:45:30 +0900

    3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -4,15 +4,26 @@ int main(int argc, const char* argv[]) {
     const char* filename = "numbers.txt";
 
     FILE* fp = fopen(filename, "r");
-    if (fp == NULL) {
+    if(fp == NULL) {
         return 0;
     }
 
+    int num;
+    int sum=0;
 
-    int sum = 0;
+    char buffer[256];
 
-
+    while(!feof(fp)) {
+        if(fscanf(fp, "%d", &num) == 1) {
+            sum += num;
+        } 
+        else {
+            fscanf(fp, "%s", buffer);
+            fprintf(stderr, "invalid: %s\n", buffer);
+        }
+    }
+    
     printf("sum: %d\n", sum);
-    fclose(fp);
+    fcose(fp);
     return 0;
 }
