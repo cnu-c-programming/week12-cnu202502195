4ff5f26ddd70ca548828eeb13c3329129c224f33
cnu202502195 <202502195@o.cnu.ac.kr>
2026-05-26 13:56:41 +0900

    2-5.c

--- code/2-5.c
+++ code/2-5.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
 int main(int argc, const char* argv[]) {
-    if (argc < 3)
+    if(argc < 3)
         return 0;
 
+    FILE* fp_src = fopen(argv[1], "r");
+    FILE* fp_dst = fopen(argv[2], "w");
 
+    char buffer[256];
+
+    while(fgets(buffer, sizeof(buffer), fp_src) != NULL) {
+        fputs(buffer, fp_dst);
+    }
 }
-
